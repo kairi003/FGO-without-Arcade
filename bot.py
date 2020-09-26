db726e278ca2a692955325a54b455c5a6d294c7d
kairi <kairi.satellite@gmail.com>
2020-09-26 19:10:41 +0900

    [update]Add Quote Tweet

--- bot.py
+++ bot.py
@@ -19,11 +19,18 @@ def get_api():
     return api
 
 
-class FixedStatus(tweepy.Status):
+class ExStatus(tweepy.Status):
     def __init__(self, status):
         self.__dict__.update(status.__dict__)
         self.full_text = self._full_text()
         self.is_arcade = self._is_arcade(self.full_text)
+        self.url = self._get_status_url()
+    
+    def _get_status_url(self):
+        name = self.author.screen_name
+        id_ = self.id_str
+        url = f'https://twitter.com/{name}/status/{id_}'
+        return url
 
     def _full_text(self):
         if hasattr(self, 'extended_tweet'):
@@ -40,9 +47,11 @@ class FixedStatus(tweepy.Status):
 
 class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status_ori):
-        status = FixedStatus(status_ori)
+        status = ExStatus(status_ori)
         if status.author.id_str == TARGET_ID and not status.is_arcade:
             status.retweet()
+            self.api.update_status(status.url)
+
 
 
 def main():
